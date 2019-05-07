9c5f3acdd8c4787ab33ad3ebecb76c30d062f8dc
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2019-05-07 17:18:34 -0500

    Agrego Configuracion para ejecutar aplicacion en diferentes ambientes usando dotenv

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from './config.service';
 
 @Injectable()
 export class AppService {
+  private appName: string;
+  private portApi: number;
+
+  constructor(config: ConfigService) {
+    this.appName = config.appName;
+    this.portApi = config.portApi;
+  }
+
+  getAppName(): string {
+    return this.appName;
+  }
+
+  getPortApi(): number {
+    return this.portApi;
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
