b09467cb2a05fe982f52b0c3f7991de31ebcad89
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2019-05-07 17:51:06 -0500

    Agrego configuracion de tecnicas de seguridad

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
+import * as rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  /**
+   * CORS: https://github.com/expressjs/cors#configuration-options
+   */
+  const app = await NestFactory.create(AppModule, { cors: true });
+  /**
+   * Security: https://docs.nestjs.com/v5/techniques/security
+   */
+  app.use(helmet());
+  /**
+   * CSRF: https://docs.nestjs.com/v5/techniques/security
+   */
+  // app.use(csurf());
+  /**
+   * To protect from brute-force attacks: https://docs.nestjs.com/v5/techniques/security
+   */
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
+  // Run app
   await app.listen(3000);
 }
 bootstrap();
